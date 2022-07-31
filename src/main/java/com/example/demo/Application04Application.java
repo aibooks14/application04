94451ab1ff9cf2b77d114681fb293d2e1baf08da
aibooks14 <101002119+aibooks14@users.noreply.github.com>
2022-07-31 16:48:09 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/Application04Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application04Application.class, args);
+	}
+
+}
